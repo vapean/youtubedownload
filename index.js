160aef6a7c25daa9083d2045a7ad93a96fbb67c7
Antonio Vallejo <antonio.vapei@gmail.com>
2020-04-21 20:22:04 +0200

    videos de una url  con titulo, url y canal

---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const inquirer = require('inquirer');
+// creamos una funcion autoejecutada para tener un contexto asincrono
+(async () => {
+    // obtenemos el html de la página con axios
+    const response = await axios.get('https://www.youtube.com/results?search_query=calle+13')
+    // const response = await axios.get('https://www.youtube.com/feed/trending')
+
+    // iniciamos cheerio pasando el html obtenido de axios
+    const $ = cheerio.load(response.data);
+    // obtenemos la lista de videos
+    const listaVideos = $('.yt-lockup-content');
+
+    const videos = [];
+    // iteramos la lista de videos
+    listaVideos.each((i, elem) => {
+        // de cada uno de los elementos html buscamos los elementos con la clase que tiene el título (de ellos sacamos el texto )
+        const title = $(elem).find('.yt-uix-tile-link').text().trim();
+        // controlamos si es algun elemento intercalado que no corresponda
+        if (!title) return
+        const url = $(elem).find('.yt-uix-tile-link').attr('href')
+        const chanel = $(elem).find('.yt-lockup-byline').children('a').text().trim()
+
+        videos.push({
+            title: title,
+            url: 'http://www.youtube.com' + url,
+            chanel: chanel
+        })
+
+
+
+    })
+    console.log(videos)
+
+})()
