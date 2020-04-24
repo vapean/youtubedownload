ad58008568dbc3e41833f5e96a85352fa25836c4
Antonio Vallejo <antonio.vapei@gmail.com>
2020-04-24 14:13:36 +0200

    completado a nivel consola

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const inquirer = require('inquirer');
+const youtubedl = require('youtube-dl');
+const fs = require('fs');
+
 // creamos una funcion autoejecutada para tener un contexto asincrono
 (async () => {
     // obtenemos el html de la página con axios
@@ -19,20 +22,18 @@ const inquirer = require('inquirer');
         const title = $(elem).find('.yt-uix-tile-link').text().trim();
         // controlamos si es algun elemento intercalado que no corresponda
         if (!title) return
+
         const url = $(elem).find('.yt-uix-tile-link').attr('href')
         const chanel = $(elem).find('.yt-lockup-byline').children('a').text().trim()
+        if (!chanel) return
 
         videos.push({
             title: title,
             url: 'http://www.youtube.com' + url,
             chanel: chanel
         })
-
-
-
     })
 
-    
     inquirer.prompt([
         {
             type: 'list',
@@ -45,7 +46,30 @@ const inquirer = require('inquirer');
         }
     ]).then(answers => {
         console.log(answers['selectedVideo']);
-        
+        videoDowload(answers['selectedVideo'])
+
     })
 
 })()
+
+function videoDowload(videoInfo) {
+    const video = youtubedl(videoInfo.url,
+        // Optional arguments passed to youtube-dl.
+        ['--format=18'],
+        // Additional options can be given for calling `child_process.execFile()`.
+        { cwd: __dirname })
+
+    // Will be called when the download starts.
+    video.on('info', function (info) {
+        console.log('Download started')
+        console.log('filename: ' + info._filename)
+        console.log('size: ' + info.size)
+    })
+
+    video.pipe(fs.createWriteStream(videoInfo.title + '.mp4'))
+
+    video.on('end', function () {
+        console.log('finished downloading!')
+    })
+
+}
